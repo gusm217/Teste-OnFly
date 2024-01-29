070bff2dbba199ae13841b28430ff4755c2e497f
gusm217 <gusm217@gmail.com>
2024-01-29 11:30:38 -0300

    chore: add jest configs and env example

---
+++ jest.setup.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false
+});
+
+sequelize.sync();
+
+module.exports = sequelize;
