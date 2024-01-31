eca70b1ad4261f1507bc284be793dd514a0213e3
gusm217 <gusm217@gmail.com>
2024-01-31 09:23:34 -0300

    feat: add authentication middleware

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@ const express = require('express');
 const expensesRoutes = require('./api/routes/expensesRoutes');
 const userRoutes = require('./api/routes/userRoutes');
 const authRoute = require('./api/routes/authRoute');
+const authMiddleware = require('./api/middlewares/authMiddleware')
 
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
-app.use('/expenses', expensesRoutes);
+app.use('/expenses', authMiddleware, expensesRoutes);
 app.use('/users', userRoutes);
 app.use('/login', authRoute);
 
